687c67d5c319c2bc7d67351b8f2d770ebba22015
TomTheToad <tomthetoad951@gmail.com>
2017-02-19 17:11:19 -0500

    Updated/repaired module name in CoreDataStack.

--- VirtualTourist/CoreDataStack.swift
+++ VirtualTourist/CoreDataStack.swift
@@ -14,7 +14,7 @@ class CoreDataStack {
     
     // Fields
     // Set Module name
-    static let moduleName = "VirtualTourist"
+    static let moduleName = "VirtualTouristModel"
     
     // URL for CoreDataStack module "momd" file
     var modelURL: URL {
