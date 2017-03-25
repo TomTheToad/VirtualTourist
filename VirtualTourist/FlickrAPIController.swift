8face662eda180b79560922c18bf40bae0b1d013
TomTheToad <tomthetoad951@gmail.com>
2017-03-24 22:14:48 -0400

    Another round of updates to FlickrAPIController. Refined FlickrDictToURL() function with more robust optional chaining.

--- VirtualTourist/FlickrAPIController.swift
+++ VirtualTourist/FlickrAPIController.swift
@@ -115,18 +115,34 @@ class FlickrAPIController {
         https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_[mstzb].jpg
         or
         https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
+         
+         
+         s	small square 75x75
+         q	large square 150x150
+         t	thumbnail, 100 on longest side
+         m	small, 240 on longest side
+         n	small, 320 on longest side
+         -	medium, 500 on longest side
+         z	medium 640, 640 on longest side
+         c	medium 800, 800 on longest side†
+         b	large, 1024 on longest side*
+         h	large 1600, 1600 on longest side†
+         k	large 2048, 2048 on longest side†
+         o	original image, either a jpg, gif or png, depending on source format
+
          */
         
         var urlsArray = [String]()
         
         for image in dictionary {
-            // change all to guard
-            let farmID = image.value(forKey: "farm")
-            let serverID = image.value(forKey: "server")
-            let secret = image.value(forKey: "secret")
-            let id = image.value(forKey: "id")
-            let url = "https://farm\(farmID!).staticflickr.com/\(serverID!)/\(id!)_\(secret!).jpg"
-            urlsArray.append(url)
+            
+            if let farmID = image.value(forKey: "farm"),
+                let serverID = image.value(forKey: "server"),
+                let secret = image.value(forKey: "secret"),
+                let id = image.value(forKey: "id") {
+                    let url = "https://farm\(farmID).staticflickr.com/\(serverID)/\(id)_\(secret)_t.jpg"
+                    urlsArray.append(url)
+            }
         }
         
         return urlsArray
